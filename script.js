cc12d54d26439481eb5250e1b4f45e81bc17fcec
Christine Yi <christineyi@Christines-MacBook-Pro.local>
2017-03-08 10:32:02 -0500

    removed script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// General Assembly, WDI (Web Development Immersive) Remote, Cohort 06 ()
-// Copyright (C) 2017 Christine Yi under the GNU General Public License.
-// See LICENSE for details.
-"use strict";
-/// DO NOT EDIT ABOVE THIS LINE ///
