1f44b78899caa657f6b51205d14b124f8bc1b7cf
trilltown <phil.awich@gmail.com>
2019-11-02 17:22:40 -0400

    Solve 1-4

---
+++ script.js
@@ -0,0 +1,25 @@
+// ### Challenge 1: Booleans
+// 1. Create a variable `a` and set it equal to `true`.
+// 2. Create a variable `b` and set it equal to `false`.
+// 3. Write a statement using `a` and `b` that evaluates to `false`.
+//   - console log the result to terminal
+// 4. Write a statement using `a` and `b` that evaluates to `true`.
+//   - console log the result to terminal
+
+let a = true; 
+let b = false; 
+console.log(a === b)
+console.log(a !== b)
+
+// For each expression below, indicate whether it is 'truthy' or 'falsey' by assigning a variable a value of either true or false:
+
+// 5. true && false
+// 6. true && 1 != 1
+// 7. "1" === 1
+// 8. !(true && false)
+// 9. false || 0 != 0
+// 10. 3 >= 3 && (!("banana" === "banana" || "javascript" === "fun"))
+// 11. null === undefined
+// 12. isNaN(undefined)
+// 13. :dart: Commit -m "Commit 1: Boolean Skills"
+
